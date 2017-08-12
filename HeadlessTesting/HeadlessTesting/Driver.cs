1b847220472f2034e21cfc90ab63d87342bd77c4
petegibbins <petegibbins@gmail.com>
2017-08-12 01:15:00 +0100

    Add files via upload

---
+++ HeadlessTesting/HeadlessTesting/Driver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net.Mime;
+using System.Reflection;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace HeadlessTesting
+{
+    public class Driver
+    {
+        public static IWebDriver Instance { get; set; }
+
+        public static void Initialize()
+        {
+            var options = new ChromeOptions();
+            // headless requires version 60+
+            options.AddArgument("headless");
+            options.AddArgument("disable-gpu");
+            // set size, just in case responsive website
+            options.AddArgument("window-size=1200x600");
+
+            // If you want to use Canary build of Chrome, specify the path
+            //options.BinaryLocation = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\..\Local\Google\Chrome SxS\Application\chrome.exe";
+            
+            // If the path to the ChromeDriver doesn't work, just download it and stick it in your path
+            Instance = new ChromeDriver("WebDrivers", options);
+
+        }
+    }
+}
---
+++ HeadlessTesting/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using HeadlessTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Driver.Initialize();
+
+            Driver.Instance.Navigate().GoToUrl("http://seleniumhq.org");
+
+            // a guarantee that the test was really executed
+            Assert.IsTrue(Driver.Instance.FindElement(By.Id("q")).Displayed);
+
+            Driver.Instance.Quit();
+        }
+    }
+}
